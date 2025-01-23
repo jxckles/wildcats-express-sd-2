6a39f06660c5d6e9aec20a2c0bd2ad1fe2cd019b
jxckles <dojolesjaier9@gmail.com>
2025-01-23 20:34:18 +0800

    Update index.jsx

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -14,16 +14,13 @@ export const Auth = () => {
                     <h1 className="wildcats-mobile">Wildcats <span className="express-mobile">Express</span></h1>
                 </div>
                 <h3>Fast. Fresh. Fierce.</h3>
-                <Link to="/login-page" className="primary-cta">Order Food</Link>
+                <Link to="/login-page" className="primary-cta">Order Here</Link>
             </div>
 
             <div className="cat">
                 <img src={catImage} alt="Photo of Wildcat chef"/>
             </div>
       </main>
-
-
-  
     </>
  
   );
